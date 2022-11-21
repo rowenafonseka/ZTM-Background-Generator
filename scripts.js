5c3589ad11ae70c676f531e6eef6391e0aead1f4
rowena fonseka <rafonseka@gmail.com>
2022-11-21 13:55:16 -0500

    added h3 text content

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const body = document.getElementById("gradient");
 const changeColor = () => {
   body.style.background =
     "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+  css.textContent = body.style.background + ";";
 };
 
 color1.addEventListener("input", changeColor);
