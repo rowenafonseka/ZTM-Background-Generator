e3d54f182c362794bca78bf7698d31394e020cd9
rowena fonseka <rafonseka@gmail.com>
2022-11-21 13:50:58 -0500

    added color gradient event listener functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
+const changeColor = () => {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+};
+
+color1.addEventListener("input", changeColor);
+
+color2.addEventListener("input", changeColor);
